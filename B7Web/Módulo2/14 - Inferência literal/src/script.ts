3876105b2605ad7acfc8d7750595051cc80345d7
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-08-26 14:56:03 -0300

    Inferência literal

---
+++ B7Web/Módulo2/14 - Inferência literal/src/script.ts
@@ -0,0 +1,21 @@
+function fazerRequisicao(url: string, method: 'GET' | "POST"){
+    //.......
+}
+
+let url = "https://google.com.br";
+let method = 'GET';
+
+fazerRequisicao(url, method);
+
+ /*ocorre o erro pq no momento a variável esta com "GET" mas ela pode mudar de valor
+uma das formas de resolver e dizer literalmente o que a variável vai receber
+
+
+type Methods = 'GET' | 'POST';
+
+let url = "https://google.com.br";
+let method: Methods = 'GET';
+
+fazerRequisicao(url, method);
+*/
+
