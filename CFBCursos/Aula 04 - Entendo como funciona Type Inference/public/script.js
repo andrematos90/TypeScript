bfb63bf6c38b512377ce1fd9000492f2f9808393
André Matos <andrermatos90@gmail.com>
2023-10-02 17:19:40 -0300

    Aula 04 - Inferenica de tipos

---
+++ CFBCursos/Aula 04 - Entendo como funciona Type Inference/public/script.js
@@ -0,0 +1,12 @@
+"use strict";
+//inferencia de tipo explicita
+let nome;
+let idade;
+let lindo;
+nome = "André";
+idade = 33;
+lindo = true;
+console.log(`meu nome é ${nome}, tenho ${idade} anos de idade. Sou lindo? ${lindo}`);
+//inferiencia automatica, no exemplo com base no valor
+let nomeCachorro = "simba";
+console.log(typeof (nomeCachorro));
---
+++ CFBCursos/Aula 04 - Entendo como funciona Type Inference/src/script.ts
@@ -0,0 +1,14 @@
+//inferencia de tipo explicita
+let nome:string;
+let idade:number;
+let lindo:boolean;
+
+nome = "André"
+idade = 33;
+lindo = true;
+
+console.log(`meu nome é ${nome}, tenho ${idade} anos de idade. Sou lindo? ${lindo}`)
+
+//inferiencia automatica, no exemplo com base no valor
+let nomeCachorro = "simba";
+console.log(typeof(nomeCachorro));
