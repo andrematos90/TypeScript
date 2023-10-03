931d0696104021f242b60e974873d2b93caf8fd7
André Matos <andrermatos90@gmail.com>
2023-10-03 15:30:13 -0300

    Aula 06 - Readonly

---
+++ CFBCursos/Aula 06 - Readonly/public/script.js
@@ -0,0 +1,5 @@
+"use strict";
+let numeros = [100, 200, 300]; // só aceita métos de leitura e nenhum que altera o array
+numeros.forEach((element) => {
+    console.log(element);
+});
---
+++ CFBCursos/Aula 06 - Readonly/src/script.ts
@@ -0,0 +1,6 @@
+let numeros : ReadonlyArray<number>=[100, 200, 300]; // só aceita métos de leitura e nenhum que altera o array
+
+numeros.forEach((element : number) => {
+    console.log(element)
+});
+
