444362c3e1110a1dfa85d1dd223b6ac7bf30a686
André Matos <andrermatos90@gmail.com>
2023-10-04 16:18:31 -0300

    Aual 06 - ReadonlyArray

--- CFBCursos/Aula 06 - Readonly/public/script.js
+++ CFBCursos/Aula 06 - Readonly/public/script.js
@@ -1,5 +1,19 @@
 "use strict";
-let numeros = [100, 200, 300]; // só aceita métos de leitura e nenhum que altera o array
+//formas de declarar o array e seu tipo
+// let numeros : Array<number>
+// let numeros : number[]
+// let numeros:(number)[]
+let arrayNormal = ["Casa", "Avião", "Mansão"];
+//esse tipo de array aceita todos os métodos de manipulação de arrays
+arrayNormal.push("Navio"); //push adiciona elemento no final do array
+arrayNormal.unshift("Bicicleta"); //unshift adiciona elemento no inicio do array
+arrayNormal.pop(); //pop exclui o ultimo elemento do array
+arrayNormal.shift(); //shift exclui elemento do inicio
+arrayNormal.forEach((element) => {
+    console.log(element);
+});
+// ReadonlyArray só aceita métodos de leitura e nenhum que altera o array
+let numeros = [100, 200, 300];
 numeros.forEach((element) => {
     console.log(element);
 });
