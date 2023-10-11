a55621db0f4798e235631f3e7973f8d5a0abc82e
André Matos <andrermatos90@gmail.com>
2023-10-11 18:15:49 -0300

    Aula 17 - método construtor

--- CFBCursos/Aula 17 - P.O.O Método Construtor/public/script.js
+++ CFBCursos/Aula 17 - P.O.O Método Construtor/public/script.js
@@ -1,22 +1,17 @@
-class Aeronave{
-    tipo:string;
-    autonomia:number;
-    fabricante:string;
-
-    constructor(tipo:string, autonomia:number, fabricante:string){
+"use strict";
+class Aeronave {
+    tipo;
+    autonomia;
+    fabricante;
+    constructor(tipo, autonomia, fabricante) {
         this.tipo = tipo;
         this.autonomia = autonomia;
         this.fabricante = fabricante;
     }
-    
 }
-
 const aeronave1 = new Aeronave("Avião", 2000, "Embraer");
 const aeronave2 = new Aeronave("Helicoptero", 500, "LockHeed Martin");
-
 console.log(`A primeira aeronave é um ${aeronave1.tipo}, com autonomia de 
  ${aeronave1.autonomia} km e fabricante é a ${aeronave1.fabricante}`);
-
- console.log(`A segunda aeronave é um ${aeronave2.tipo}, com autonomia de 
+console.log(`A segunda aeronave é um ${aeronave2.tipo}, com autonomia de 
  ${aeronave2.autonomia} km e fabricante é a ${aeronave2.fabricante}`);
-
