be088443f8af2f2b947770a3dbbf9920c6dcd76e
André Matos <andrermatos90@gmail.com>
2023-10-16 20:19:10 -0300

    Aula 23 - Herança parte 5

---
+++ CFBCursos/Aula 23 - P.O.O Herança parte 5/src/script.ts
@@ -0,0 +1,131 @@
+
+class Conta {
+    protected nome: string;     
+    protected numero: number;   
+    protected saldo:number;
+
+    constructor(nome: string) {     
+        this.nome = nome;           
+        this.numero = this.gerarNumeroConta();   
+        this.saldo = 0;
+    }
+
+    gerarNumeroConta(): number {    
+        return Math.floor(Math.random() * 100000) + 1;
+    }
+
+    protected info(){
+        console.log(`nome do titular: ${this.nome}`);
+        console.log(`numero da conta: ${this.numero}`);
+        console.log();
+        console.log("------------------------------");
+       
+    }
+
+   
+}
+
+
+class ContaPF extends Conta {
+    public cpf: number;   
+
+    constructor(cpf: number, nome: string) {  
+        super(nome);     
+        this.cpf = cpf;   
+    
+    }
+    public info(): void {
+        super.info();
+        console.log(`numero CPF....: ${this.cpf}`)
+    }
+
+    public deposito(deposito:number):number{
+        if(deposito > 0 || deposito < 1000){
+             this.saldo += deposito;
+             console.log("depositado com sucesso!")
+        }else{
+            console.log("deposito indisponivel!")
+        }
+        return this.saldo;
+    }
+
+    public saque(saque:number):number{
+        if(saque <= this.saldo && saque <1000){
+            this.saldo -= saque;
+            console.log("saque realizado!");
+        }else{
+            console.log("saque indisponível")
+        }
+        return this.saldo;
+    }
+
+    public mostraSaldo():void{
+        console.log(`saldo disponível: ${this.saldo}`)
+    }
+
+}
+
+
+class ContaPJ extends Conta {
+    public cnpj: number;  
+
+    constructor(cnpj: number, nome: string) {  nome
+        super(nome);       
+        this.cnpj = cnpj;  
+         
+    }
+    protected info(): void {
+        super.info();
+        console.log(`numero CNPJ: ${this.cnpj}`)
+    }
+
+    public deposito(deposito:number):number{
+        if(deposito > 0 || deposito < 10000){
+             this.saldo += deposito;
+             console.log("Depositado com sucesso!")
+        }
+        else{
+            console.log("depoisto indisponível")
+        }
+
+        return this.saldo;
+    }
+
+    public saque(saque:number):number{
+       if(saque <= this.saldo && saque <10000){
+        this.saldo -= saque;
+        console.log("saque realizado!")
+       }else{
+        console.log("saque indisponível");
+       }
+       return this.saldo;
+    }
+
+    public mostraSaldo():void{
+        console.log(`saldo diponível: ${this.saldo}`)
+    }
+
+}
+
+const cliente1 = new ContaPF(65151, "fulano"); 
+const cliente2 = new ContaPJ(454352, "ciclano");  
+
+
+cliente1.info() //método pode ser acessado pq é public
+//cliente2.info()  gera erro pq método é protected
+
+cliente1.deposito(10000);
+cliente2.deposito(1000000);
+
+cliente1.saque(500);
+cliente2.saque(38000);
+
+cliente1.mostraSaldo();
+cliente2.mostraSaldo();
+
+
+
+
+
+
+
